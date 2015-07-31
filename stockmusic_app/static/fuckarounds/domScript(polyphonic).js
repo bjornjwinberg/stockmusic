a8bfe73bdeae59eba375a7bfcf3ec53999ce0cd5
bjorn <bjorn@descendo>
2015-07-31 17:55:15 -0400

    end of day friday. added minor scale on down moves. add visual events on turning points next.

--- stockmusic_app/static/fuckarounds/domScript(polyphonic).js
+++ stockmusic_app/static/fuckarounds/domScript(polyphonic).js
@@ -1,104 +1,18 @@
 "use strict"
 
-var major = [261.626, 293.665, 329.628, 349.228, 391.995, 440.000, 493.883];
-var scaleCounter = 0;
 var mySynth = myStuff.create();
-var upDays = [];
-var downDays = [];
-
-
-function dataGrapher(data, minness, maxness, longness) {
-    $("#lineBackground").empty();
-
-    var svg = d3.select("#lineBackground")
-      .append("svg")
-      .attr("width", 1000)
-      .attr("height", 310)
-      .attr("id", "visualization");
-
-    var x = d3.scale.linear().domain([0, longness]).range([0, 1000]);
-    var y = d3.scale.linear().domain([maxness, minness]).range([10, 300]);
-    var line = d3.svg.line()
-      .interpolate("linear")
-      .x(function(d,i) {return x(i);})
-      .y(function(d) {return y(d);});
-
-    var path = svg.append("path")
-      .attr("d", line(data))
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", "5")
-      .attr("fill", "white");
-
-    var totalLength = path.node().getTotalLength();
-    path
-      .attr("stroke-dasharray", totalLength + " " + totalLength)
-      .attr("stroke-dashoffset", totalLength)
-      .transition()
-        .duration(0)
-        .ease("linear")
-        .attr("stroke-dashoffset", 0);
-};
 
 $(document).ready(function() {
 
-    $("body").on("finished", function(event, killInterval){
-        window.clearInterval(killInterval);
-        myStuff.stop()
-    });
-
-    $("#stock_form").on("submit", function() {
-        event.preventDefault();
-        var speed = Number($(this).children('input[name="tempo"]').val());
-        var formData = $(this).serialize();
-
-        $.get($(this).attr("action"), formData, function(data) {
-            var minness = Math.min.apply(null, data["quote"]);
-            var maxness = Math.max.apply(null, data["quote"]);
-            var longness = data["quote"].length
-
-            var worker = new Worker('/static/stockmusic_app/task.js');
-
-
-            worker.addEventListener('message', function(e) {
-                myStuff.play();
-                mySynth.set("carrier.freq", e.data[0]);
-                dataGrapher(data["quote"].slice(0, e.data[1]+2), minness, maxness, longness);
-                if(e.data[1]+2 == longness) {
-                    setTimeout(function() {
-                        myStuff.stop();
-                    }, 1000);
-                };
-            }, false);
-            dataGrapher(data["quote"].slice(0, 1), minness, maxness, longness);
-            worker.postMessage([data["frequency_sequence"], speed]);
-        });
-        $(this).children("input[name]").val("");
-    });
     $("#start").on("click", function() {
         myStuff.play();
     });
-    $("#pitch_up").on("click", function() {
-        if (scaleCounter == 6) {
-            scaleCounter = -1
-        };
-        scaleCounter += 1;
-        scaleCounter = Math.abs(scaleCounter);
-        mySynth.set("carrier.freq", major[scaleCounter % major.length]);
-    });
-    $("#pitch_down").on("click", function() {
-        if (scaleCounter == 0) {
-            scaleCounter = 7
-        };
-        scaleCounter -= 1;
-        scaleCounter = Math.abs(scaleCounter);
-        mySynth.set("carrier.freq", major[scaleCounter % major.length]);
-    });
+
     $("#shut_up").on("click", function() {
         myStuff.stop()
     });
 });
 
-// http://bl.ocks.org/duopixel/4063326 (chart you modified)
 
 
 
--- stockmusic_app/views.py
+++ stockmusic_app/views.py
@@ -6,17 +6,22 @@ from django.views.generic import View
 
 
 def make_notes(quotes):
-    major = [261.626, 293.665, 329.628, 349.228, 391.995, 440.000, 493.883]
+    # major = [261.626, 293.665, 329.628, 349.228, 391.995, 440.000, 493.883]
+    major = [261.626, 329.628, 391.995, 493.883]
+    minor = [261.626, 311.127, 391.995, 415.305]
     frequency_sequence = []
+    # pitch_and_mood = []
     octave = 1
     scaleCounter = 0
 
     for index in range(len(quotes)):
+        # pitch_and_mood = []
+
         if index + 1 == len(quotes):
             continue
         else:
             if quotes[index] < quotes[index+1]:
-                if scaleCounter == 6:
+                if scaleCounter == 3:
                     scaleCounter = -1
                     if octave == 0.5:
                         octave = 1
@@ -27,14 +32,14 @@ def make_notes(quotes):
                 frequency_sequence.append(octave * major[scaleCounter % len(major)])
             else:
                 if scaleCounter == 0:
-                    scaleCounter = 7
+                    scaleCounter = 4
                     if octave > 1:
                         octave -= 1
                     else:
                         octave = 0.5
                 scaleCounter -= 1
                 scaleCounter = abs(scaleCounter)
-                frequency_sequence.append(octave * major[scaleCounter % len(major)])
+                frequency_sequence.append(octave * minor[scaleCounter % len(major)])
     return frequency_sequence
 
 
@@ -61,3 +66,19 @@ class YahooView(View):
         quote = q['query']['results']['quote']
         dumberer = [float(quote[idx]['Adj_Close']) for idx in range(len(quote)-1, -1, -1)]
         return JsonResponse({"quote": dumberer, "frequency_sequence": make_notes(dumberer)})
+
+
+
+
+
+
+            # if index+2 < len(quotes):
+            #     if quotes[index] < quotes[index+1] and quotes[index+1] < quotes[index+2]:
+            #         pitch_and_mood.append("happy")
+            #     else:
+            #         pitch_and_mood.append("normal")
+
+            # if quotes[index] > quotes[index+1] and quotes[index+1] > quotes[index+2]:
+            #     pitch_and_mood.append("sad")
+            # else:
+            #     pitch_and_mood.append("normal")
